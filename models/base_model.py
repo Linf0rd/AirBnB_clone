a4ec437f0f0c35f29d0a45617606410b07b6119b
Linf0rd <linfordsnagger@gmx.com>
2023-10-15 16:38:09 +0200

    Update BaseModel class with object saving
    Modified the 'BaseModel' class to include saving objects to the storage after updating them. Added a 'save' method that updates 'updated_at' and saves the object using 'models.storage.save()'. This enhances object persistence and ensures changes are saved.

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 Class BaseModel
 """
-
+import models
 import uuid
 from datetime import datetime
 
@@ -29,6 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
 
     def __str__(self):
@@ -43,9 +44,10 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the updated_at attribute with the current datetime.
+        Updates the updated_at attribute with the current datetime and saves the object.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
