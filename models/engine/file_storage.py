7ab55222598ff9b7e0989c88848a0b9ae87bfd9b
Linf0rd <linfordsnagger@gmx.com>
2023-10-15 16:42:34 +0200

    Define the FileStorage class
    Created the 'FileStorage' class, which is responsible for serializing instances to a JSON file and deserializing a JSON file into instances. It provides methods for managing objects and storing them in a JSON file.

---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Defines the FileStorage class.
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes
+    JSON file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists).
+        """
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    cls = eval(class_name)
+                    obj = cls(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
