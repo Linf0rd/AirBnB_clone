27c011b4b3331d3d3049bff2b826a2d4193c4ad7
Linf0rd <linfordsnagger@gmx.com>
2023-10-17 14:55:25 +0200

    Implement FileStorage class for multiple classes
    Implemented the FileStorage class, which serializes instances to a JSON file and deserializes them back to instances. This class supports multiple classes, including BaseModel and User, allowing the storage and retrieval of objects for various types. The 'all', 'new', 'save', and 'reload' methods provide the essential functionality for data management.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,16 @@
 Defines the FileStorage class.
 """
 import json
+import os
+from datetime import datetime
+import uuid
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage(object):
